8f29742d8b5b34a07c2ff47b2d61617109889f2c
Josiah Laivins <jokellum@northstate.net>
2019-10-07 16:41:34 -0400

    Azure Pipeline Testing

--- build/azure_pipeline_helper.sh
+++ build/azure_pipeline_helper.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Install pybullet
 git clone https://github.com/benelot/pybullet-gym.git
 cd pybullet-gym
 pip install -e .
+cd ../
+
+# Install gym_maze
+git clone https://github.com/MattChanTK/gym-maze.git
+cd gym-maze
+python setup.py install
+cd ../
+
