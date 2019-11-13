07ffd4f3ca96efb36e86c2fbcb6b2353c9ee2b62
Josiah Laivins <jokellum@northstate.net>
2019-11-13 18:04:09 -0500

    More test fixing

--- fast_rl/tests/test_dqn.py
+++ fast_rl/tests/test_dqn.py
@@ -46,3 +46,34 @@ def test_dqn_models_cartpole(model_cls, s_format, experience):
         del learn
         del model
         del data
+
+
+@pytest.mark.usefixtures('skip_performance_check')
+@pytest.mark.parametrize(["model_cls", "s_format", 'experience'],
+                         list(product(params_dqn, params_state_format, params_experience)))
+def test_dqn_models_mountaincar(model_cls, s_format, experience):
+    group_interp = GroupAgentInterpretation()
+    for i in range(5):
+        memory = experience(memory_size=1000000, reduce_ram=True)
+        model = partial(model_cls, memory=memory, layers=[64, 64], discount=0.99, grad_clip=1,
+                        optimizer=torch.optim.RMSprop)
+
+        if isinstance(model, DQN):
+            model = partial(model, lr=0.001)
+        else:
+            model = partial(model, lr=0.001, copy_over_frequency=300)
+
+        print('\n')
+        data = MDPDataBunch.from_env('MountainCar-v0', render='rgb_array', bs=32, add_valid=False, feed_type=s_format)
+        learn = AgentLearner(data, model(data))
+        learn.fit(450)
+
+        meta = f'{experience.__name__}_{"FEED_TYPE_STATE" if s_format == FEED_TYPE_STATE else "FEED_TYPE_IMAGE"}'
+        interp = AgentInterpretation(learn, ds_type=DatasetType.Train)
+        interp.plot_rewards(cumulative=True, per_episode=True, group_name=meta)
+        group_interp.add_interpretation(interp)
+        group_interp.to_pickle(f'../docs_src/data/mountaincar_{model.name.lower()}/', f'{model.name.lower()}_{meta}')
+
+        del learn
+        del model
+        del data
