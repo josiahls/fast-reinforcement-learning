Fixed: - Normalizing cuda error

fast_rl/agents/ddpg.py
@@ -127,7 +127,7 @@ class CNNCritic(nn.Module):
 class DDPG(BaseAgent):
 
     def __init__(self, data: MDPDataBunch, memory=None, tau=1e-3, discount=0.99,
-                 lr=1e-3, actor_lr=1e-4, exploration_strategy=None):
+                 lr=1e-3, actor_lr=1e-4, opt=None, exploration_strategy=None, **kwargs):
         """
         Implementation of a discrete control algorithm using an actor/critic architecture.
 
@@ -147,7 +147,7 @@ class DDPG(BaseAgent):
             discount: Determines the amount of discounting the existing Q reward.
             lr: Rate that the opt will learn parameter gradients.
         """
-        super().__init__(data)
+        super().__init__(data, **kwargs)
         self.name = 'DDPG'
         self.lr = lr
         self.discount = discount
@@ -159,8 +159,8 @@ class DDPG(BaseAgent):
         self.action_model = self.initialize_action_model([400, 300], data)
         self.critic_model = self.initialize_critic_model([400, 300], data)
 
-        self.opt = OptimWrapper.create(Adam, lr=actor_lr, layer_groups=[self.action_model])
-        self.critic_optimizer = OptimWrapper.create(Adam, lr=lr, layer_groups=[self.critic_model])
+        self.opt = OptimWrapper.create(ifnone(opt, Adam), lr=actor_lr, layer_groups=[self.action_model])
+        self.critic_optimizer = OptimWrapper.create(ifnone(opt, Adam), lr=lr, layer_groups=[self.critic_model])
 
         self.t_action_model = deepcopy(self.action_model)
         self.t_critic_model = deepcopy(self.critic_model)

fast_rl/agents/dqn.py
@@ -105,7 +105,7 @@ def get_action_dqn_cnn(layers, action: Action, state: State, activation=nn.ReLU,
 class DQN(BaseAgent):
     def __init__(self, data: MDPDataBunch, memory=None, lr=0.00025, discount=0.95, grad_clip=5,
                  max_episodes=None, exploration_strategy=None, use_embeddings=False, layers=None,
-                 loss_func=None, optimizer=None, attempt_normalize=True):
+                 loss_func=None, opt=None, attempt_normalize=True):
         """Trains an Agent using the Q Learning method on a neural net.
 
         Notes:
@@ -131,7 +131,7 @@ class DQN(BaseAgent):
         self.loss_func = ifnone(loss_func, F.mse_loss)
         self.memory = ifnone(memory, ExperienceReplay(10000))
         self.action_model = self.initialize_action_model(ifnone(layers, [64, 64]), data.train_ds)
-        self.opt = OptimWrapper.create(ifnone(optim.Adam, optimizer), lr=self.lr, layer_groups=[self.action_model])
+        self.opt = OptimWrapper.create(ifnone(optim.Adam, opt), lr=self.lr, layer_groups=[self.action_model])
         self.learner_callbacks += [partial(BaseDQNCallback, max_episodes=max_episodes)] + self.memory.callbacks
         self.exploration_strategy = ifnone(exploration_strategy, GreedyEpsilon(epsilon_start=1, epsilon_end=0.1,
                                                                                decay=0.001,

fast_rl/tests/test_ddpg_models.py
@@ -1,29 +1,43 @@
-from collections import Collection
 from functools import partial
 from itertools import product
 
 import pytest
-from fastai.basic_train import LearnerCallback
+from fastai.basic_train import torch, DatasetType
 
 from fast_rl.agents.ddpg import DDPG
-from fast_rl.core.Envs import Envs
-from fast_rl.core.data_block import FEED_TYPE_IMAGE, FEED_TYPE_STATE, MDPDataBunch
-from fast_rl.core.agent_core import ExperienceReplay, OrnsteinUhlenbeck
+from fast_rl.core.agent_core import ExperienceReplay, PriorityExperienceReplay
 from fast_rl.core.basic_train import AgentLearner
+from fast_rl.core.data_block import FEED_TYPE_STATE, MDPDataBunch
+from fast_rl.core.metrics import RewardMetric, EpsilonMetric
+from fast_rl.core.train import GroupAgentInterpretation, AgentInterpretation
 
-# params_dqn = [DDPG]
-# params_envs = ['Pendulum-v0', 'CarRacing-v0']
-# params_state_format = [FEED_TYPE_STATE]#, FEED_TYPE_IMAGE]
-#
-#
-# @pytest.mark.parametrize(["env", "model", "s_format"], list(product(params_envs, params_dqn, params_state_format)))
-# def test_ddpg_models(env, model, s_format):
-#     model = partial(model, memory=ExperienceReplay(memory_size=1000, reduce_ram=True))
-#     data = MDPDataBunch.from_env(env, render='rgb_array', max_steps=20, bs=4, add_valid=False, feed_type=s_format)
-#     learn = AgentLearner(data, model(data))
-#     if s_format == FEED_TYPE_STATE: learn.fit(3)
-#     else: learn.fit(1)
-#     data.train_ds.env.close()
-#     del learn
-#     del model
-#     del data
+params_dqn = [DDPG]
+params_experience = [PriorityExperienceReplay, ExperienceReplay]
+params_state_format = [FEED_TYPE_STATE]#, FEED_TYPE_IMAGE]
+
+
+@pytest.mark.usefixtures('skip_performance_check')
+@pytest.mark.parametrize(["model_cls", "s_format", 'experience'],
+                         list(product(params_dqn, params_state_format, params_experience)))
+def test_ddpg_models_pendulum(model_cls, s_format, experience):
+    group_interp = GroupAgentInterpretation()
+    for i in range(5):
+        memory = experience(memory_size=1000000, reduce_ram=True)
+        model = partial(model_cls, memory=memory, opt=torch.optim.RMSprop)
+
+        print('\n')
+        data = MDPDataBunch.from_env('Pendulum-v0', render='human', bs=32, add_valid=False,
+                                     feed_type=s_format)
+        model = model(data)
+        learn = AgentLearner(data, model, callback_fns=[RewardMetric, EpsilonMetric])
+        learn.fit(450)
+
+        meta = f'{experience.__name__}_{"FEED_TYPE_STATE" if s_format == FEED_TYPE_STATE else "FEED_TYPE_IMAGE"}'
+        interp = AgentInterpretation(learn, ds_type=DatasetType.Train)
+        interp.plot_rewards(cumulative=True, per_episode=True, group_name=meta)
+        group_interp.add_interpretation(interp)
+        group_interp.to_pickle(f'../docs_src/data/pendulum_{model.name.lower()}/', f'{model.name.lower()}_{meta}')
+
+        del learn
+        del model
+        del data

fast_rl/tests/test_dqn.py
@@ -26,7 +26,7 @@ def test_dqn_models_minigrids(model_cls, s_format, experience):
     for i in range(5):
         memory = experience(memory_size=1000000, reduce_ram=True)
         model = partial(model_cls, memory=memory, layers=[64, 64], discount=0.99, grad_clip=1,
-                        optimizer=torch.optim.RMSprop,
+                        opt=torch.optim.RMSprop,
                         exploration_strategy=GreedyEpsilon(epsilon_start=1, epsilon_end=0.1, decay=0.00001))
 
         if model_cls is DQN:
@@ -60,7 +60,7 @@ def test_dqn_models_cartpole(model_cls, s_format, experience):
     for i in range(5):
         memory = experience(memory_size=1000000, reduce_ram=True)
         model = partial(model_cls, memory=memory, layers=[64, 64], discount=0.99, grad_clip=1,
-                        optimizer=torch.optim.RMSprop)
+                        opt=torch.optim.RMSprop)
 
         if model_cls is DQN:
             model = partial(model, lr=0.001)
@@ -92,7 +92,7 @@ def test_dqn_models_lunarlander(model_cls, s_format, experience):
     for i in range(5):
         memory = experience(memory_size=1000000, reduce_ram=True)
         model = partial(model_cls, memory=memory, layers=[128, 64], discount=0.99, grad_clip=1,
-                        optimizer=torch.optim.RMSprop)
+                        opt=torch.optim.RMSprop)
 
         if model_cls is DQN:
             model = partial(model, lr=0.001)
@@ -124,7 +124,7 @@ def test_dqn_models_mountaincar(model_cls, s_format, experience):
     for i in range(5):
         memory = experience(memory_size=1000000, reduce_ram=True)
         model = partial(model_cls, memory=memory, layers=[24, 12], discount=0.99, grad_clip=5,
-                        optimizer=torch.optim.RMSprop,
+                        opt=torch.optim.RMSprop,
                         exploration_strategy=GreedyEpsilon(epsilon_start=1, epsilon_end=0.1, decay=0.0001))
 
         if model_cls is DQN: