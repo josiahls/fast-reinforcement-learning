d4dd34c7809c1aa212bf68f1efb4a9ae8303e99d
Josiah Laivins <jokellum@northstate.net>
2019-12-15 22:03:52 -0500

    Removed: - old code from README. Revisions coming.

--- tests/test_agent_core.py
+++ tests/test_agent_core.py
@@ -1,6 +1,6 @@
-import pytest
-
-from fast_rl.core.data_block import MDPDataBunch
-from fast_rl.core.agent_core import PriorityExperienceReplay
-from fast_rl.core.basic_train import AgentLearner
-
+# import pytest
+#
+# from fast_rl.core.data_block import MDPDataBunch
+# from fast_rl.core.agent_core import PriorityExperienceReplay
+# from fast_rl.core.basic_train import AgentLearner
+#
