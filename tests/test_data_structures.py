893a333b7657a28f1b475d272389dfe8561732e2
Josiah Laivins <jokellum@northstate.net>
2019-12-15 22:38:56 -0500

    Removed: - old code from README. Revisions coming.

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -1,15 +1,15 @@
-# from fast_rl.core.data_structures import print_tree, SumTree
-#
-#
-# def test_sum_tree_with_max_size():
-#     memory = SumTree(10)
-#
-#     values = [1, 1, 1, 1, 1, 1]
-#     data = [f'data with priority: {i}' for i in values]
-#
-#     for element, value in zip(data, values):
-#         memory.add(value, element)
-#
-#     print_tree(memory)
-#
-#
+from fast_rl.core.data_structures import print_tree, SumTree
+
+
+def test_sum_tree_with_max_size():
+    memory = SumTree(10)
+
+    values = [1, 1, 1, 1, 1, 1]
+    data = [f'data with priority: {i}' for i in values]
+
+    for element, value in zip(data, values):
+        memory.add(value, element)
+
+    print_tree(memory)
+
+
